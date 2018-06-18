d46b798161d5467749141f5677d21af1092d6b34
Javier Infante <jabi@irontec.com>
2018-06-18 11:34:32 +0200

    light theme

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import 'highlight.js/styles/atom-one-dark.css'
 /**
  * Select current revel.js theme
  */
-import 'reveal.js/css/theme/moon.css';
+import 'reveal.js/css/theme/simple.css';
 
 //import '../reveal-irontec-theme/css/styleLight.css'
 
